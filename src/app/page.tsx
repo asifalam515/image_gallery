77b29f902f3da657fcca88f8a41c928585009fa0
asifalam515 <asibulalam515@gmail.com>
2025-04-09 08:33:24 +0600

    upload image component created

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import UploadImages from "@/components/UploadImages";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <UploadImages></UploadImages>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/UploadImages.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UploadImages = () => {
+  return (
+    <div>
+      <h1>Upload Your Favorite Image</h1>
+    </div>
+  );
+};
+
+export default UploadImages;
