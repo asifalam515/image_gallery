1dcec86913895fc43b4bbd132bcada89fb4b901b
asifalam515 <asibulalam515@gmail.com>
2025-04-09 12:46:38 +0600

    navbar added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginTs from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@typescript-eslint": eslintPluginTs,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Disable the unused-vars rule globally
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/components/UploadImages.tsx
+++ src/components/UploadImages.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+"use client";
+import { CldUploadWidget } from "next-cloudinary";
 
 const UploadImages = () => {
   return (
     <div>
       <h1>Upload Your Favorite Image</h1>
+      <CldUploadWidget signatureEndpoint="/api/sign-image">
+        {({ open }) => {
+          return (
+            <button
+              className="bg-indigo-500 rounded py-2 px-2 text-white"
+              onClick={() => open()}
+            >
+              Upload an Image
+            </button>
+          );
+        }}
+      </CldUploadWidget>
     </div>
   );
 };
