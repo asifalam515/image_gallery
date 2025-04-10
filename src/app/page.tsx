7902d911668b672193658f6ede545ed68aaa0d19
asifalam515 <asibulalam515@gmail.com>
2025-04-10 15:07:12 +0600

    footer component updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-// import Images from "@/components/Images";
 import Gallery from "@/components/Gallery";
-import UploadImages from "@/components/UploadImages";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Home</h1>
-      <br />
       <Gallery></Gallery>
     </div>
   );
